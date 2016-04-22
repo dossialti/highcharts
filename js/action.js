8dbf1b010ea47ae9cf1695541670aa9199a9816f
Librae <librae8226@gmail.com>
2016-04-22 15:28:36 +0800

    fix datetime format and do cleanups

--- js/action.js
+++ js/action.js
@@ -1,33 +1,5 @@
-function httpGetAsync(theUrl, callback) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
-	}
-	xmlHttp.open("GET", theUrl, true); // true for asynchronous
-	xmlHttp.send(null);
-}
-
-function cb(response) {
-	var lines = response.split('\n');
-	for(var i = 0; i < lines.length; i++) {
-		console.log('[' + i + '] ' + lines[i]);
-	}
-}
-
-function loop(series) {
-	var x = (new Date()).getTime();
-	var y = Math.random();
-	series.addPoint([x, y], true, true);
-}
-
 function retrieveAndConvertData(url, callback) {
     console.log("in");
-    /* hijack here before dev finished */
-    /*
-    $.getJSON('http://biloba.accrete.org:8000/highcharts/raw.json', function(json) {
-        callback(json);
-    });
-    */
     var json = {
         "xData": [],
         "datasets": [{
@@ -86,7 +58,8 @@ function retrieveAndConvertData(url, callback) {
             if (lines[i].length != 0) {
                 console.log('[' + i + '] ' + lines[i]);
 		var raw = JSON.parse(lines[i]);
-                json.xData.push(raw[0].timestamp);
+		var dt = Date.parse(raw[0].timestamp);
+                json.xData.push(dt);
 		json.datasets.forEach(function(obj) {
 	            switch (obj.name) {
                         case "Light":
@@ -212,6 +185,7 @@ function run() {
                     },
                     xAxis: {
                         type: 'datetime',
+                        tickPixelInterval: 50,
                         crosshair: true,
                         events: {
                             setExtremes: syncExtremes
@@ -344,6 +318,12 @@ function run_click() {
     });
 }
 
+function loop(series) {
+	var x = (new Date()).getTime();
+	var y = Math.random();
+	series.addPoint([x, y], true, true);
+}
+
 function run_interval() {
     $(document).ready(function () {
         Highcharts.setOptions({
@@ -416,61 +396,6 @@ function run_interval() {
             }]
         });
     });
-}
-
-function run_old() {
-	var ts = new Date().toString();
-	console.log(ts);
-	var url = 'http://biloba.accrete.org:8000/highcharts/raw.part.txt';
-	httpGetAsync(url, cb);
-	$('#container').highcharts({
-		title: {
-			text: 'Monthly Average Temperature',
-			x: - 20
-		},
-		subtitle: {
-			text: 'Source: WorldClimate.com',
-			x: - 20
-		},
-		xAxis: {
-			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		},
-		yAxis: {
-			title: {
-				text: 'Temperature (°C)'
-			},
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
-		},
-		tooltip: {
-			valueSuffix: '°C'
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle',
-			borderWidth: 0
-		},
-		series: [{
-			name: 'Tokyo',
-			data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-		},
-		{
-			name: 'New York',
-			data: [ - 0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-		},
-		{
-			name: 'Berlin',
-			data: [ - 0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-		},
-		{
-			name: 'London',
-			data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-		}]
-	});
 }
 
 $(function() {
